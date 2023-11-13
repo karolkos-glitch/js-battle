7cd53f11d5bfcbabb97b9069a7d296c4789f0823
Karol <karol99kos@gmail.com>
2023-11-13 22:46:34 +0100

    Styles - add tailwind, daisy-ui, make two pages for solid and react components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    solidJs({include: ['**/solid/*']}), 
-    react({include: ['**/react/*']})
-  ]
+  integrations: [solidJs({
+    include: ['**/solid/*']
+  }), react({
+    include: ['**/react/*']
+  }), tailwind()]
 });
--- src/components/solid/STableRoot.tsx
+++ src/components/solid/STableRoot.tsx
@@ -1,7 +1,42 @@
 const STableRoot = () => {
   return <div>
-    Solid Table Root
-  </div>
+   <h1 class="text-5xl font-bold">Solid Component</h1>
+   <div class="overflow-x-auto">
+    <table class="table">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+  
+      <tr>
+        <th>2</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+      </tr>
+      
+      <tr>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>
 }
 
 export default STableRoot;
