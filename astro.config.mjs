62ce7ae4ce7936e6b3e4c9be1540044a535a468d
Karol Kosek <karol99kos@gmail.com>
2024-04-05 22:55:41 +0200

    Remove unused files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
